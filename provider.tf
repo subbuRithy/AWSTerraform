9292ed0722405dda3174e5c4336f1d791481c591
Subbu <thota.manyam@gmail.com>
2024-03-14 11:20:35 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -11,3 +11,14 @@ provider "aws" {
   
 }
 
+
+
+
+resource "aws_instance" "test" {
+    ami = "ami-02a2af70a66af6dfb"
+    instance_type = "t2.micro"
+
+    tags = {
+    Name = "subbu_instance"
+}
+}
